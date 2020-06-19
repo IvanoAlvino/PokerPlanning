4d766c587f42972a7696dc60b421510339069280
Nikita Zhuravlev <nikita.zhuravlev@navvis.com>
2020-06-19 16:24:39 +0200

    Add card list component

--- frontend/src/app/card/card.component.ts
+++ frontend/src/app/card/card.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
-  selector: 'app-card',
+  selector: 'card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
+  @Input()
+  public value: number;
+
+  @Input()
+  public selected: boolean = false;
+
+  @Output()
+  public select = new EventEmitter<boolean>();
+
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public toggle(): void {
+    this.select.emit(this.selected);
   }
 
 }
