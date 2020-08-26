ab928bf906b6e822bcf6e05faadc34a12576461d
Ivano Alvino <ivano.alvino@navvis.com>
2020-08-26 11:03:21 +0200

    Sort the User estimated in descending order

--- frontend/src/app/users-list/users-list.component.ts
+++ frontend/src/app/users-list/users-list.component.ts
@@ -11,8 +11,18 @@ export class UsersListComponent
 	/**
 	 * The list of users to display.
 	 */
+	private _userList: UserEstimate[];
+
 	@Input()
-	public userList: UserEstimate[];
+	public set userList(value: UserEstimate[])
+	{
+		this._userList = this.getSortedEstimatesDESC(value);
+	}
+
+	public get userList(): UserEstimate[]
+	{
+		return this._userList;
+	}
 
 	/**
 	 * Whether the currently logged in user is the room moderator.
@@ -31,4 +41,27 @@ export class UsersListComponent
 	 */
 	@Input()
 	public isVotingOngoing: boolean;
+
+	/**
+	 * Clone the provided estimates array and sort it by estimate value, in a descending order.
+	 * @param estimates The array of estimates to sort
+	 */
+	public getSortedEstimatesDESC(estimates: UserEstimate[]): UserEstimate[]
+	{
+		// Clone the array not to tamper with original data
+		const sortedEstimate = [...estimates];
+		return sortedEstimate.sort((firstEstimate, secondEstimate) => {
+			if (firstEstimate.estimate === "?" || !firstEstimate.estimate)
+			{
+				return 1;
+			}
+
+			if (secondEstimate.estimate === "?" || !secondEstimate.estimate)
+			{
+				return -1;
+			}
+
+			return parseFloat(secondEstimate.estimate) - parseFloat(firstEstimate.estimate);
+		});
+	}
 }
