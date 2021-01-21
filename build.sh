da9f643c37210760f0781e6184fe4df8b35cbbeb
Sam Dowsett <sam.dowsett@navvis.com>
2021-01-21 21:48:52 +0100

    Convert to jib this is cool :)

--- build.sh
+++ build.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 
-cd frontend || exit
-npm run build
-cp dist/frontend/* ../backend/src/main/resources/static
-cd ../backend || exit
-mvn clean install
-cd .. || exit
-docker build -t navvis-poker-planning .
+cd backend || exit
+mvn compile jib:build
