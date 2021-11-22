a6da3d560cffece4655cdef4aac85366470d830e
Ivano Alvino <ivano.alvino@gmail.com>
2021-11-23 00:45:14 +0100

    Update entire angular stack to latest version

--- frontend/src/app/fireworks/fireworks/fireworks.component.spec.ts
+++ frontend/src/app/fireworks/fireworks/fireworks.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { FireworksComponent } from './fireworks.component';
 
@@ -6,7 +6,7 @@ describe('FireworksComponent', () => {
   let component: FireworksComponent;
   let fixture: ComponentFixture<FireworksComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ FireworksComponent ]
     })
